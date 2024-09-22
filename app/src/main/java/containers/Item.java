d199a123068a8264ff6127f618745f4ae9aaf779
parker <parker.e.davis@gmail.com>
2024-09-22 14:52:14 -0400

    Item is now a self-made struct

--- app/src/main/java/containers/Item.java
+++ app/src/main/java/containers/Item.java
@@ -2,5 +2,12 @@ package containers;
 
 public class Item
 {
-    
+    public String id;
+    public int amount;
+
+    Item(String _id, int _amount)
+    {
+        id = _id;
+        amount = _amount;
+    }
 }
